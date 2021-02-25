5b08e668ca59baf11197d1a3c8aef305bac1d147
Candice Agard <candiceagard@candices-mbp.myfiosgateway.com>
2021-02-25 14:08:32 -0500

    History storing link bullet points, sum showing currently showing up NAN

--- index.js
+++ index.js
@@ -1,13 +1,45 @@
 document.addEventListener("DOMContentLoaded", () => {
-    const userChoice = document.querySelector('.dice-input');
-    const btnRoll = document.querySelector('.dice-para');
+    const input = document.querySelector('#number-Dice');
+    const inputNum = Number(input)
+    const diceLanding = document.querySelector('#rollingdice');
+    const p = document.querySelector('#sum-para');
 
-    btnRoll.addEventListener('click', function() {
-        const diceHistory = document.createElement('li');
-        const listContent = document.createTextNode(userChoice.value);
+    let arr = []
 
-        diceHistory.appendChild(listContent);
+    diceLanding.addEventListener('click', () => {
+        if(input.value !== " "){
+            const dice = [
+                '⚀',
+                '⚁',
+                '⚂',
+                '⚃',
+                '⚄',
+                '⚅'
+            ]
 
+            arr = []
+            const afterRoll = document.querySelector('#landedDice')
+
+
+            for(i = 0; i < input.value; i++){
+                let newArr = dice[Math.floor(Math.random()* 6) + 1]
+                arr += Number[newArr]
+                afterRoll.innerHTML = arr[newArr]
+                if( afterRoll.textContent !== 0){
+                    p.textContent += inputNum + `Roll`
+                }
+                console.log(arr)
+            }
+
+            const storedHistory = document.querySelector('#passed-history');
+            const li = document.createElement('li')
+                li.textContent = arr.value
+                storedHistory.appendChild(li)
+                 if(afterRoll.textContent == 1){
+                     li.textContent = []
+                 }else if(p.textContent > 1){
+                     li.textContent = li.textContent + afterRoll.textContent
+                 }
+        }
     })
-
 })
