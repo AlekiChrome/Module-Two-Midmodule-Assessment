0e7115b7d3b4eddfd807517d4a07f1dc6f2d6c9c
Candice Agard <candiceagard@candices-mbp.myfiosgateway.com>
2021-02-25 16:38:37 -0500

    Scramble and restart

--- index.js
+++ index.js
@@ -1,50 +1,60 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const input = document.querySelector('#number-Dice');
-    const inputNum = Number(input)
-    const diceLanding = document.querySelector('#rollingdice');
-    const p = document.querySelector('#sum-para');
+const roll = (x) => {
+    let dice = ['\u2680', '\u2681', '\u2682', '\u2683', '\u2684', '\u2685'];
+    let results = '';
+    for (let i = 0; i < x; i++) {
+        results += dice[Math.floor(Math.random()*6)] + ' ';
+    }
+    return results;
+}
 
-    let arr = []
+const doRoll = (amount, result) => {
+    document.getElementById(result).innerHTML =
+        roll(parseInt(document.getElementById(amount).value));
+}
 
-    diceLanding.addEventListener('click', () => {
-        if(input.value !== " "){
-            const dice = [
-                '⚀ ',
-                '⚁ ',
-                '⚂ ',
-                '⚃ ',
-                '⚄ ',
-                '⚅ '
-            ]
+// document.addEventListener("DOMContentLoaded", () => {
+//     const input = document.querySelector('#number-Dice');
+//     const inputNum = Number(input)
+//     const diceLanding = document.querySelector('#rollingdice');
+//     const p = document.querySelector('#sum-para');
 
-            if (input.value === 1) {
-                 dice[0]
-            }
+//     let arr = []
 
-            arr = []
-            const afterRoll = document.querySelector('#landedDice')
+//     diceLanding.addEventListener('click', () => {
+//         if(input.value !== " "){
+//             const dice = [
+//                 '⚀ ',
+//                 '⚁ ',
+//                 '⚂ ',
+//                 '⚃ ',
+//                 '⚄ ',
+//                 '⚅ '
+//             ]
 
-
-            for(i = 0; i < input.value; i++){
-                let newArr = dice[Math.ceil(Math.random()* dice.length)]
-                arr += newArr
-                afterRoll.textContent = arr
-                if( afterRoll.textContent !== 0){
-                    p.textContent += inputNum
-                }
-                console.log(newArr);
-            }
+//             arr = []
+//             const afterRoll = document.querySelector('#landedDice')
 
 
-            const storedHistory = document.querySelector('#passed-history');
-            const li = document.createElement('li')
-                li.textContent = arr.value
-                storedHistory.appendChild(li)
-                 if(afterRoll.textContent == 1){
-                     li.textContent = []
-                 }else if(p.textContent > 1){
-                     li.textContent = li.textContent + afterRoll.textContent
-                 }
-        }
-    })
-})
+//             for(i = 0; i < input.value; i++){
+//                 let newArr = dice[Math.ceil(Math.random()* dice.length)]
+//                 arr += newArr
+//                 afterRoll.textContent = arr
+//                 if( afterRoll.textContent !== 0){
+//                     p.textContent += inputNum
+//                 }
+//                 console.log(newArr);
+//             }
+
+
+//             const storedHistory = document.querySelector('#passed-history');
+//             const li = document.createElement('li')
+//                 li.textContent = arr.value
+//                 storedHistory.appendChild(li)
+//                  if(afterRoll.textContent == 1){
+//                      li.textContent = []
+//                  } else if (p.textContent > 1){
+//                      li.textContent = li.textContent + afterRoll.textContent
+//                  }
+//         }
+//     })
+// })
