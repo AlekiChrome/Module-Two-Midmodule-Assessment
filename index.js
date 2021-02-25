8290064ee3aa40c9d3c3162b66ba177db6ee8281
Candice Agard <candiceagard@Candices-MacBook-Pro.local>
2021-02-25 11:21:31 -0500

    btn event listener and inputs added

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
 document.addEventListener("DOMContentLoaded", () => {
-    
+    const userChoice = document.querySelector('.dice-input');
+    const btnRoll = document.querySelector('.dice-para');
+
+    btnRoll.addEventListener('click', function() {
+        const diceHistory = document.createElement('li');
+        const listContent = document.createTextNode(userChoice.value);
+
+        diceHistory.appendChild(listContent);
+
+    })
+
 })
