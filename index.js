c5bde18135350b16dfce4b26bf873a56a3c3c6f0
Candice Agard <candiceagard@Candices-MBP.home>
2021-03-02 12:39:50 -0500

    attempting to redo

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+// My SMART Goal is ....
+
+// I will structure my code to pass the cypress test and store the history of the dice into it's accurate div container
+
 const roll = (x) => {
     let dice = ['\u2680', '\u2681', '\u2682', '\u2683', '\u2684', '\u2685'];
     let results = '';
@@ -11,50 +15,3 @@ const doRoll = (amount, result) => {
     document.getElementById(result).innerHTML =
         roll(parseInt(document.getElementById(amount).value));
 }
-
-// document.addEventListener("DOMContentLoaded", () => {
-//     const input = document.querySelector('#number-Dice');
-//     const inputNum = Number(input)
-//     const diceLanding = document.querySelector('#rollingdice');
-//     const p = document.querySelector('#sum-para');
-
-//     let arr = []
-
-//     diceLanding.addEventListener('click', () => {
-//         if(input.value !== " "){
-//             const dice = [
-//                 '⚀ ',
-//                 '⚁ ',
-//                 '⚂ ',
-//                 '⚃ ',
-//                 '⚄ ',
-//                 '⚅ '
-//             ]
-
-//             arr = []
-//             const afterRoll = document.querySelector('#landedDice')
-
-
-//             for(i = 0; i < input.value; i++){
-//                 let newArr = dice[Math.ceil(Math.random()* dice.length)]
-//                 arr += newArr
-//                 afterRoll.textContent = arr
-//                 if( afterRoll.textContent !== 0){
-//                     p.textContent += inputNum
-//                 }
-//                 console.log(newArr);
-//             }
-
-
-//             const storedHistory = document.querySelector('#passed-history');
-//             const li = document.createElement('li')
-//                 li.textContent = arr.value
-//                 storedHistory.appendChild(li)
-//                  if(afterRoll.textContent == 1){
-//                      li.textContent = []
-//                  } else if (p.textContent > 1){
-//                      li.textContent = li.textContent + afterRoll.textContent
-//                  }
-//         }
-//     })
-// })
